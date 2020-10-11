9d48a696ea3a82e7ca63e068660818cc6990cc43
Stas Mikhailov <mik.stanis@yandex.ru>
2020-10-11 15:27:12 +0300

    Updates

---
+++ precache-manifest.b9f1896c6732a39233cbc6113496d55b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c9c2693d275f8480a94957d5431202",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed496862f191f262488d",
+    "url": "./static/css/main.5ad7fe7a.chunk.css"
+  },
+  {
+    "revision": "32210566bc2130818b2e",
+    "url": "./static/js/2.685e74ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.685e74ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed496862f191f262488d",
+    "url": "./static/js/main.d01119e4.chunk.js"
+  },
+  {
+    "revision": "aa973a27221b7e99b786",
+    "url": "./static/js/runtime-main.e48908e9.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "./static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "./static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "./static/media/activatedLike.5bbb05f7.svg"
+  },
+  {
+    "revision": "055023bb31ce22a900ab927d03a48bc4",
+    "url": "./static/media/close-icon.055023bb.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "./static/media/deactivatedLike.a8acc871.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "./static/media/mesto-logo.c6f11019.svg"
+  },
+  {
+    "revision": "437bf923043170a3a43208fee9d00652",
+    "url": "./static/media/mobile-close-icon.437bf923.svg"
+  },
+  {
+    "revision": "5df9ea80259cbb131b25d5648d497d91",
+    "url": "./static/media/mobile-menu.5df9ea80.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "./static/media/trash-button.4220563a.svg"
+  }
+]);
